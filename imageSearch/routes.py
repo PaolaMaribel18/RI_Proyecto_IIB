6a0025887be2243766a2353210551ac0c5cfda7e
PaolaMaribel18 <102838577+PaolaMaribel18@users.noreply.github.com>
2024-08-04 07:24:49 -0500

    chore: Update file paths for data and images in app and routes

--- imageSearch/routes.py
+++ imageSearch/routes.py
@@ -1,12 +1,13 @@
 import os
 import numpy as np
 
+
 def list_images(base_path):
     image_paths = []
     for root, dirs, files in os.walk(base_path):
         for file in files:
             if file.lower().endswith(('.png', '.jpg', '.jpeg')):
-                image_paths.append(os.path.join(root, file))
+                image_paths.append(os.path.join(root, file).replace("\\", "/"))
     return image_paths
 
 base_image_path = 'static/images/caltech101/101_ObjectCategories'
@@ -14,3 +15,7 @@ image_paths = list_images(base_image_path)
 
 # Guarda las rutas en un archivo
 np.save('data/image_paths.npy', image_paths)
+
+print("Rutas guardadas exitosamente.")
+print("Primeras 5 rutas:")
+print(image_paths[:5])
