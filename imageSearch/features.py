3e2ec037fca17acea1c859ef6652a10e08d2de11
PaolaMaribel18 <102838577+PaolaMaribel18@users.noreply.github.com>
2024-08-07 02:16:24 -0500

    chore: Update image search app with VGG16 model and save image classes

--- imageSearch/features.py
+++ imageSearch/features.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 from tensorflow.keras.applications import InceptionV3
+from tensorflow.keras.applications import VGG16
 from tensorflow.keras.models import Model
 import numpy as np
 import tensorflow_datasets as tfds
@@ -14,12 +15,17 @@ data_dir = r'C:\Users\Paola\Downloads\caltech101'
     with_info=True,
     as_supervised=True,
     data_dir=data_dir,
-    download=True
+    download=False # Cambiar a True para descargar
 )
+
+name_classes = dataset_info.features['label'].names
+print ("Número de clases:", len(name_classes))
+print ("Clases:", name_classes)
+np.save('name_classes.npy', name_classes)
 
 # Preprocesar imágenes
 def preprocess_image(image, label):
-    image = tf.image.resize(image, (299, 299))  # InceptionV3 usa 299x299
+    image = tf.image.resize(image, (224, 224))  # InceptionV3 usa 299x299
     image = tf.cast(image, tf.float32) / 255.0
     return image, label
 
@@ -27,29 +33,36 @@ train_dataset = train_dataset.map(preprocess_image).shuffle(1000).batch(32)
 test_dataset = test_dataset.map(preprocess_image).batch(32)
 
 # Configurar el modelo InceptionV3
-base_model = InceptionV3(weights='imagenet', include_top=False, input_shape=(299, 299, 3))
+#base_model = InceptionV3(weights='imagenet', include_top=False, input_shape=(299, 299, 3))
+#model = Model(inputs=base_model.input, outputs=base_model.layers[-1].output)
+base_model = VGG16(weights='imagenet', include_top=False, input_shape=(224, 224, 3))
 model = Model(inputs=base_model.input, outputs=base_model.layers[-1].output)
 
-# Función para extraer características
+# Función para extraer características e imagenes
 def extract_features(dataset):
     features = []
     labels = []
+    imgs = []
     for images, lbls in dataset:
+        imgs.append(images)
         feature_maps = model.predict(images)
         features.append(feature_maps)
         labels.append(lbls.numpy())
-    return features, labels
+    return features, labels,imgs
 
 # Extraer características para train y test
-train_features, train_labels = extract_features(train_dataset)
-test_features, test_labels = extract_features(test_dataset)
+train_features, train_labels, train_img = extract_features(train_dataset)
+test_features, test_labels, test_img = extract_features(test_dataset)
 
+#Indexación
 # Aplanar las características para el índice
 train_features_flat = np.array([feature.flatten() for batch in train_features for feature in batch])
 train_labels_flat = np.array([label for batch in train_labels for label in batch])
+train_img_flat = np.array([img for batch in train_img for img in batch])
 
 # Guardar el archivo
 np.save('data/train_features.npy', train_features_flat)
 np.save('data/train_labels.npy', train_labels_flat)
+np.save('data/train_img.npy', train_img_flat)
 
 print("Características y etiquetas guardadas exitosamente.")
